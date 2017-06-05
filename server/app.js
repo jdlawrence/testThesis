79ac07220b4396261e4bddad4b23f69bd0a84d12
jdlawrence <jamil.lawrence@gmail.com>
2017-06-05 14:41:12 -0700

    Removes testing comment in server and adds one in React component

--- server/app.js
+++ server/app.js
@@ -20,7 +20,6 @@ app.use(middleware.flash());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Comment for testing
 app.use('/', routes.auth);
 app.use('/api', routes.api);
 app.use('/api/profiles', routes.profiles);
